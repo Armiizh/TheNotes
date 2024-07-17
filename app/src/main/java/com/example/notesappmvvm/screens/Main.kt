b56f52c7cb8248429bc2a1470c82769526596549
Artemii Zh <zhulkin2013@yandex.ru>
2024-07-17 19:45:35 +0400

    Create ViewModel, Model, Room

--- app/src/main/java/com/example/notesappmvvm/screens/Main.kt
+++ app/src/main/java/com/example/notesappmvvm/screens/Main.kt
@@ -1,10 +1,13 @@
 package com.example.notesappmvvm.screens
 
+import android.app.Application
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
@@ -14,18 +17,31 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.notesappmvvm.MainViewModel
+import com.example.notesappmvvm.MainViewModelFactory
+import com.example.notesappmvvm.model.Note
 import com.example.notesappmvvm.navigation.NavRoute
 import com.example.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
 @Composable
 fun MainScreen(navController: NavHostController) {
+
+    val context = LocalContext.current
+    val mViewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+
+    val notes = mViewModel.readTest.observeAsState(listOf()).value
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -39,30 +55,13 @@ fun MainScreen(navController: NavHostController) {
             }
         },
         content = { PaddingValues ->
-            Column(
+            LazyColumn(
                 modifier = Modifier
                     .padding(PaddingValues(vertical = 8.dp, horizontal = 12.dp))
             ) {
-                NoteItem(
-                    title = "Note 1",
-                    subtitle = "Subtitle for Note 1",
-                    navController = navController
-                )
-                NoteItem(
-                    title = "Note 2",
-                    subtitle = "Subtitle for Note 2",
-                    navController = navController
-                )
-                NoteItem(
-                    title = "Note 3",
-                    subtitle = "Subtitle for Note 3",
-                    navController = navController
-                )
-                NoteItem(
-                    title = "Note 4",
-                    subtitle = "Subtitle for Note 4",
-                    navController = navController
-                )
+                items(notes) { note ->
+                    NoteItem(note = note, navController = navController)
+                }
             }
         }
     )
@@ -70,8 +69,7 @@ fun MainScreen(navController: NavHostController) {
 
 @Composable
 fun NoteItem(
-    title: String,
-    subtitle: String,
+    note: Note,
     navController: NavHostController
 ) {
     Card(
@@ -85,13 +83,13 @@ fun NoteItem(
     ) {
         Text(
             modifier = Modifier.padding(start = 8.dp, top = 8.dp, end = 8.dp),
-            text = title,
+            text = note.title,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold
         )
         Text(
             modifier = Modifier.padding(start = 8.dp, bottom = 8.dp, end = 8.dp),
-            text = subtitle
+            text = note.subtitle
         )
     }
 }
