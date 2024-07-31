0ddf21525862bb966cbab66986d00ff7292bd5da
Artemii Zh <zhulkin2013@yandex.ru>
2024-07-31 18:38:21 +0400

    add new UI AddScreen

--- app/src/main/java/com/example/notesappmvvm/MainActivity.kt
+++ app/src/main/java/com/example/notesappmvvm/MainActivity.kt
@@ -5,90 +5,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import com.example.notesappmvvm.navigation.NavRoute
 import com.example.notesappmvvm.navigation.NotesNavHost
 import com.example.notesappmvvm.ui.theme.NotesAppMVVMTheme
-import com.example.notesappmvvm.utils.DB_TYPE
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
             NotesAppMVVMTheme {
                 val context = LocalContext.current
                 val mViewModel: MainViewModel =
                     viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
                 val navController = rememberNavController()
-
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(horizontal = 16.dp),
-                                    horizontalArrangement = Arrangement.SpaceBetween
-                                ) {
-                                    Text(text = "Notes App")
-                                    if (DB_TYPE.value.isNotEmpty()) {
-                                        Icon(
-
-                                            imageVector = Icons.AutoMirrored.Filled.ExitToApp,
-                                            contentDescription = "",
-                                            modifier = Modifier.clickable {
-                                                mViewModel.signOut {
-                                                    navController.navigate(NavRoute.Login.route) {
-                                                        popUpTo(NavRoute.Login.route) {
-                                                            inclusive = true
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        )
-                                    }
-                                }
-                            },
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                            )
-                        )
-                    },
-                    content = { paddingValues ->
-                        Surface(
-                            modifier = Modifier
-                                .padding(paddingValues)
-                                .fillMaxWidth(),
-                            color = MaterialTheme.colorScheme.background
-                        ) {
-                            NotesNavHost(mViewModel, navController)
-                        }
-                    }
-                )
+                NotesNavHost(mViewModel, navController)
             }
         }
     }
--- app/src/main/java/com/example/notesappmvvm/screens/Main.kt
+++ app/src/main/java/com/example/notesappmvvm/screens/Main.kt
@@ -1,7 +1,9 @@
 package com.example.notesappmvvm.screens
 
+import android.app.Application
 import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -9,19 +11,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -31,6 +42,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.notesappmvvm.MainViewModel
+import com.example.notesappmvvm.MainViewModelFactory
 import com.example.notesappmvvm.R
 import com.example.notesappmvvm.model.Note
 import com.example.notesappmvvm.navigation.NavRoute
@@ -41,12 +53,52 @@ import com.example.notesappmvvm.utils.TYPE_ROOM
 import me.saket.swipe.SwipeAction
 import me.saket.swipe.SwipeableActionsBox
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavHostController, viewModel: MainViewModel) {
 
     val notes = viewModel.readAllNotes().observeAsState(listOf()).value
+    val context = LocalContext.current
+    val mViewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
 
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                start = 2.dp,
+                                end = 12.dp
+                            ),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "Notes", style = MaterialTheme.typography.headlineLarge)
+                        if (DB_TYPE.value.isNotEmpty()) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                                contentDescription = "",
+                                modifier = Modifier.clickable {
+                                    mViewModel.signOut {
+                                        navController.navigate(NavRoute.Login.route) {
+                                            popUpTo(NavRoute.Login.route) {
+                                                inclusive = true
+                                            }
+                                        }
+                                    }
+                                }
+                            )
+                        }
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
@@ -58,13 +110,20 @@ fun MainScreen(navController: NavHostController, viewModel: MainViewModel) {
                 )
             }
         },
-        content = { PaddingValues ->
-            LazyColumn(
-                modifier = Modifier
-                    .padding(PaddingValues(vertical = 6.dp, horizontal = 12.dp))
+        content = { paddingValues ->
+            Surface(
+                modifier = Modifier.padding(paddingValues)
             ) {
-                items(notes) { note ->
-                    NoteItem(note = note, navController = navController, viewModel)
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(
+                            vertical = 6.dp,
+                            horizontal = 12.dp
+                        )
+                ) {
+                    items(notes) { note ->
+                        NoteItem(note = note, navController = navController, viewModel)
+                    }
                 }
             }
         }
@@ -87,6 +146,7 @@ fun NoteItem(
     val pin = swipeToPin()
 
     SwipeableActionsBox(
+        modifier = Modifier.clipSwipeActionShape(RoundedCornerShape(16.dp)),
         startActions = listOf(pin),
         endActions = listOf(remove),
         swipeThreshold = 100.dp,
@@ -157,8 +217,7 @@ private fun swipeToPin(): SwipeAction {
 private fun swipeToRemove(viewModel: MainViewModel, note: Note): SwipeAction {
     val remove = SwipeAction(
         onSwipe = {
-            viewModel.deleteNote(note = note, onSuccess = { Log.d("123", "222") })
-            Log.d("checkData", "Заметка удалилась")
+            viewModel.deleteNote(note = note, onSuccess = {})
         },
         icon = {
             Icon(
@@ -170,6 +229,13 @@ private fun swipeToRemove(viewModel: MainViewModel, note: Note): SwipeAction {
             )
         },
         background = colorResource(id = R.color.milkRed)
+
     )
     return remove
+}
+
+fun Modifier.clipSwipeActionShape(shape: Shape): Modifier {
+    return this.then(
+        Modifier.clip(shape)
+    )
 }
